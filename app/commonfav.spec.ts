c33927d3404e28e6b4c44c7411fb51256fc2933a
nagateja-nt <119793645+nagateja-nt@users.noreply.github.com>
2022-12-04 18:26:39 +0530

    Delete app directory

--- app/commonfav.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Commonfav } from './commonfav';
-
-describe('Commonfav', () => {
-  it('should create an instance', () => {
-    expect(new Commonfav()).toBeTruthy();
-  });
-});
--- app/commonfav.ts
+++
@@ -1,6 +0,0 @@
-export class Commonfav {
-    constructor(public packagename:string,public actions:any)
-    {
-
-    }
-}
